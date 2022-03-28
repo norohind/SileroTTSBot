56f698f410e94c9dac7ed369dcac1fe5f74d6a7d
norohind <60548839+norohind@users.noreply.github.com>
2022-03-28 23:53:41 +0300

    Add presence with guilds count

--- cogs/BotInformation.py
+++ cogs/BotInformation.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-
-from discord.ext import commands
+import discord
+from discord.ext import commands, tasks
 from discord.ext.commands import Context
 import datetime
 import time
@@ -10,6 +10,7 @@ class BotInformation(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot: commands.Bot = bot
         self.start_time: float = time.time()
+        self.presence_task.start()
 
     @commands.command('info')
     async def get_info(self, ctx: Context):
@@ -30,6 +31,15 @@ Invite link: https://discord.com/oauth2/authorize?client_id={self.bot.user.id}&s
         uptime = time.time() - self.start_time
         uptime_str = datetime.timedelta(seconds=int(uptime))
         await ctx.send(f"Application is up for {str(uptime_str)}")
+
+    @tasks.loop(seconds=120)
+    async def presence_task(self) -> None:
+        presence = discord.Game(f"{len(self.bot.guilds)} guilds | @{self.bot.user.name} help")
+        await self.bot.change_presence(activity=presence)
+
+    @presence_task.before_loop
+    async def presence_task_wait_ready(self):
+        await self.bot.wait_until_ready()
 
 
 async def setup(bot):
